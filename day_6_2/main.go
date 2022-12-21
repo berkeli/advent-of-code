89fdbce3b54b865b40c0e8631e47a365044b768a
Berkeli Halmyradov <berkelih@gmail.com>
2022-12-21 19:22:44 +0000

    day 6

---
+++ day_6_2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.ReadFile("./input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	input := string(f)
+
+	curr := ""
+
+	for i, ch := range input {
+		if len(curr) == 13 && !contains(curr, ch) {
+			fmt.Println(i + 1)
+			return
+		}
+
+		if !contains(curr, ch) {
+			curr += string(ch)
+		} else {
+			j := strings.IndexRune(curr, ch)
+			curr = input[i-len(curr)+j+1 : i+1]
+		}
+	}
+}
+
+func contains(a string, b rune) bool {
+	for _, c := range a {
+		if c == b {
+			return true
+		}
+	}
+	return false
+}
